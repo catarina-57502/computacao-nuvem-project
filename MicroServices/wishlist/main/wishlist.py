29cbf6b025f56a0339e581a9998a54d27cf6f84a
catarina-57502 <fc57502@alunos.fc.ul.pt>
2022-03-27 16:27:22 +0100

    wishlist com containers do servidor e app a comunicar e funções

--- MicroServices/wishlist/main/wishlist.py
+++ MicroServices/wishlist/main/wishlist.py
@@ -1,21 +1,72 @@
 from concurrent import futures
-import os
+import random
 
 import grpc
-from wishlist_pb2 import *
-from wishlist_pb2_grpc import WishlistStub
+import grpc_interceptor
 from grpc_interceptor import ExceptionToStatusInterceptor
 from grpc_interceptor.exceptions import NotFound
 
+from wishlist_pb2 import *
+import wishlist_pb2_grpc
+import pymongo
+from pymongo import MongoClient
+
+def get_table(db,table):
+    return db[table]
+
+client = MongoClient('172.20.0.2', 27017 ,username='admin', password='admin' )
+db = client['steam']
+gamesDB = get_table(db,"Games")
+
+def createdoc(request):
+    return {
+        "url":request.url,
+        "types":request.types,
+        "name":request.name,
+        "desc_snippet":request.desc_snippet,
+        "recent_reviews":request.recent_reviews,
+        "all_reviews":request.all_reviews,
+        "release_date":request.release_date,
+        "developer":request.developer,
+        "publisher":request.publisher,
+        "popular_tags":request.popular_tags,
+        "game_details":request.game_details,
+        "languages":request.languages,
+        "achievements":request.achievements,
+        "genre":request.genre,
+        "game_description":request.game_description,
+        "mature_content":request.mature_content,
+        "minimum_requirements":request.minimum_requirements,
+        "recommended_requirements":request.recommended_requirements,
+        "original_price":request.original_price,
+        "discount_price":request.discount_price
+    }
+
 class WishlistService(wishlist_pb2_grpc.WishlistServicer):
+    def AddGame(self, request, context):
+        myquery = { "url": request.url }
+        if gamesDB.count_documents(myquery) == 0:
+            gamesDB.insert_one(createdoc(request))
+            return AddGameWishResponse(message="Game Added")
+        else:
+            return AddGameWishResponse(message="Error - Game already exists")
 
-    def AddGameWish(self, request, context):
+    def DeleteGame(self, request, context):
+        myquery = { "url": request.url }
+        if gamesDB.count_documents(myquery) >= 1:
+            gamesDB.delete_one(myquery);
+            return DeleteGame(message="Game deleted")
+        else:
+            return DeleteGame(message="Error - Game not found")
 
+    def ListGames(self, request, context):
+        page = request.page
+        max_results = request.max_result
 
-    def ListGamesWish(self, request, context):
+        listGamesWish_request = ListGamesWishRequest(page=page, max_results=n)
+        g = client.ListGames(listGamesWish_request).games
 
-
-    def RemoveGameWish(self, request, context):
+        return ListGamesWishResponse(games=g)
 
 
 def serve():
@@ -23,24 +74,10 @@ def serve():
     server = grpc.server(
         futures.ThreadPoolExecutor(max_workers=10), interceptors=interceptors
     )
-    wishlist_pb2_grpc.add_WishlistServicer_to_server(
-        WishlistService(), server
+    wishlist_pb2_grpc.add_LibraryServicer_to_server(
+        LibraryService(), server
     )
-    """
-    with open("server.key", "rb") as fp:
-        server_key = fp.read()
-    with open("server.pem", "rb") as fp:
-        server_cert = fp.read()
-    with open("ca.pem", "rb") as fp:
-        ca_cert = fp.read()
-
-    creds = grpc.ssl_server_credentials(
-        [(server_key, server_cert)],
-        root_certificates=ca_cert,
-        require_client_auth=True,
-    )
-    """
-    server.add_insecure_port("[::]:27019")
+    server.add_insecure_port("[::]:50056")
     server.start()
     server.wait_for_termination()
 
