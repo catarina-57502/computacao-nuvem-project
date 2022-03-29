810f69bd4f1cf8499780496aec63a70f9c55ac08
catarina-57502 <fc57502@alunos.fc.ul.pt>
2022-03-29 21:02:26 +0100

    microserviço do search implementado v1

---
+++ MicroServices/searches/main/searches.py
@@ -0,0 +1,174 @@
+from concurrent import futures
+import random
+
+import grpc
+import grpc_interceptor
+from grpc_interceptor import ExceptionToStatusInterceptor
+from grpc_interceptor.exceptions import NotFound
+from bson import ObjectId
+from searches_pb2 import *
+import searches_pb2_grpc
+import pymongo
+from pymongo import MongoClient
+
+def get_table(db,table):
+    return db[table]
+
+myClient = MongoClient('172.21.0.2', 27017 ,username='admin', password='admin' )
+myDB = myClient["steam"]
+myGames = myDB["Games"]
+myReviews = myDB["Reviews"]
+
+def DocToGame(doc):
+    return Game(
+     _id = doc["_id"],
+     url = doc["url"],
+     types = doc["types"],
+     name = doc["name"],
+     desc_snippet = doc["desc_snippet"],
+     recent_reviews = doc["recent_reviews"],
+     all_reviews = doc["all_reviews"],
+     release_date = doc["release_date"],
+     developer = doc["developer"],
+     publisher = doc["publisher"],
+     popular_tags = doc["popular_tags"],
+     game_details = doc["game_details"],
+     languages = doc["languages"],
+     achievements = doc["achievements"],
+     genre = doc["genre"],
+     game_description = doc["game_description"],
+     mature_content = doc["mature_content"],
+     minimum_requirements = doc["minimum_requirements"],
+     recommended_requirements = doc["recommended_requirements"],
+     original_price = doc["original_price"],
+     discount_price = doc["discount_price"]
+    )
+
+def DocToReview(doc):
+    return Review(
+    review_id = doc["review_id"],
+     app_id = doc["app_id"],
+     app_name = doc["app_name"],
+     language = doc["language"],
+     review = doc["review"],
+     timestamp_created = doc["timestamp_created"],
+     timestamp_updated = doc["timestamp_updated"],
+     recommended = doc["recommended"],
+     votes_helpful = doc["votes_helpful"],
+     votes_funny = doc["votes_funny"],
+     weighted_vote_score = doc["weighted_vote_score"],
+     comment_count = doc["comment_count"],
+     steam_purchase = doc["steam_purchase"],
+     received_for_free = doc["received_for_free"],
+     written_during_early_access = doc["written_during_early_access"],
+     author_steamid = doc["author.num_games_owned"],
+     author_num_games_owned = doc["author.num_games_owned"],
+     author_num_reviews = doc["author.num_reviews"],
+     author_playtime_forever = doc["author.playtime_forever"],
+     author_playtime_last_two_weeks = doc["author.playtime_last_two_weeks"],
+     author_playtime_at_review = doc["author.playtime_at_review"],
+     author_last_played = doc["author.last_played"]
+    )
+
+class SearchesService(searches_pb2_grpc.SearchesServicer):
+    def SearchGames(self, request, context):
+
+        if(request.name == ""):
+            request.name = "NULL"
+        if(request.types == ""):
+            request.types = "NULL"
+        if(request.release_date == ""):
+            request.release_date = "NULL"
+        if(request.developer == ""):
+            request.developer = "NULL"
+        if(request.publisher == ""):
+            request.publisher = "NULL"
+        if(request.popular_tags == ""):
+            request.popular_tags = "NULL"
+        if(request.languages == ""):
+            request.languages = "NULL"
+        if(request.genre == ""):
+            request.genre = "NULL"
+        if(request.original_price == ""):
+            request.original_price = "NULL"
+
+        docGame = myGames.find( {"$or": [{"name": request.name, "types": request.types, "release_date": request.release_date,
+        "developer": request.developer, "publisher": request.publisher, "popular_tags": request.popular_tags,
+        "languages": request.languages, "genre": request.genre, "original_price": request.original_price}]}).limit(3)
+        games = []
+        dict_game = {}
+
+        for x in docGame:
+            x["_id"] = ""
+            games.append(x)
+
+        i = 0
+        for game in games:
+            str1 = "" + str(i)
+            dict_game[str1] = DocToGame(game)
+            i+=1
+
+        return SearchGamesResponse(games=dict_game)
+
+    def SearchGameById(self, request, context):
+
+        docGame = myGames.find_one({"_id": ObjectId(request.id)})
+
+        return SearchReviewResponse(game=DocToGame(docGame))
+
+
+    def SearchReviews(self, request, context):
+        if(request.app_name == ""):
+            request.app_name = "NULL"
+        if(request.timestamp_created == ""):
+            request.timestamp_created = "NULL"
+        if(request.recommended == ""):
+            request.recommended = "NULL"
+        if(request.votes_helpful == ""):
+            request.votes_helpful = "NULL"
+        if(request.votes_funny == ""):
+            request.votes_funny = "NULL"
+        if(request.comment_count == ""):
+            request.comment_count = "NULL"
+
+        docReview = myReviews.find( {"$or": [{"app_name": request.app_name, "timestamp_created": request.timestamp_created,
+        "recommended": request.recommended, "votes_helpful": request.votes_helpful, "votes_funny": request.votes_funny,
+        "comment_count": request.comment_count}]}).limit(3)
+
+        reviews = []
+        dict_review = {}
+
+        for x in docReview:
+            x["_id"] = ""
+            reviews.append(x)
+
+        i = 0
+        for review in reviews:
+            str1 = "" + str(i)
+            dict_review[str1] = DocToReview(review)
+            i+=1
+
+        return SearchReviewsResponse(reviews=dict_review)
+
+
+    def SearchReviewById(self, request, context):
+
+        docReview = myReviews.find_one({"review_id": request.id})
+
+        return SearchReviewResponse(review=DocToReview(docReview))
+
+def serve():
+    interceptors = [ExceptionToStatusInterceptor()]
+    server = grpc.server(
+        futures.ThreadPoolExecutor(max_workers=10), interceptors=interceptors
+    )
+    searches_pb2_grpc.add_SearchesServicer_to_server(
+        SearchesService(), server
+    )
+    server.add_insecure_port("[::]:50060")
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == "__main__":
+    serve()
