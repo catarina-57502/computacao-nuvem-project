19ba28a20775ba286a065624a30c4f69ed39deb3
Martim <martimmourao@gmail.com>
2022-04-07 21:45:29 +0100

    Ports Cloud setup

--- googlecloud.sh
+++ googlecloud.sh
@@ -1,7 +1,11 @@
 # Config
 export PROJECT_ID=$(gcloud info --format='value(config.project)')
 gcloud services enable cloudapis.googleapis.com  container.googleapis.com containerregistry.googleapis.com
-gcloud container clusters create cluster-steam --zone=europe-west4-a --num-nodes 2 --enable-autoscaling --min-nodes 1 --max-nodes 4
+gcloud container clusters create cluster-steam \
+--zone europe-west4-a \
+--num-nodes 2 \
+--node-locations europe-west4-a\
+--enable-autoscaling --min-nodes 1 --max-nodes 4
 gcloud auth configure-docker
 
 cd MicroServices
