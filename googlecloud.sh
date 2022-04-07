cb286fa057db5e4321c314242dfd0b97460a97fe
Martim <martimmourao@gmail.com>
2022-04-07 22:39:40 +0100

    Setup Nodes

--- googlecloud.sh
+++ googlecloud.sh
@@ -1,11 +1,7 @@
 # Config
 export PROJECT_ID=$(gcloud info --format='value(config.project)')
 gcloud services enable cloudapis.googleapis.com  container.googleapis.com containerregistry.googleapis.com
-gcloud container clusters create cluster-steam \
---num-nodes 2 \
---zone europe-west4-a \
---node-locations europe-west4-a,europe-west4-b,europe-west4-c \
---enable-autoscaling --min-nodes 1 --max-nodes 4
+gcloud container clusters create cluster-steam --num-nodes 2 --zone europe-west4-a --node-locations europe-west4-a,europe-west4-b,europe-west4-c --enable-autoscaling --min-nodes 1 --max-nodes 4
 
 gcloud auth configure-docker
 
