3648166dfba7ab3e22ef48d265c4984463d90492
b4rth0vsk1 <thomasmarques@outlook.pt>
2022-04-17 00:21:59 +0100

    HPA

--- googlecloud.sh
+++ googlecloud.sh
@@ -106,6 +106,22 @@ kubectl apply -f pv.yaml
 envsubst < "deployment.yaml" > "deploymentENV.yaml"
 kubectl apply -f deploymentENV.yaml
 
+# HPA
+kubectl autoscale deployment adminoperationsserver --cpu-percent=70 --min=1 --max=3
+kubectl autoscale deployment adminoperationsapi --cpu-percent=70 --min=1 --max=3
+kubectl autoscale deployment usermanagementserver-deployment --cpu-percent=70 --min=1 --max=3
+kubectl autoscale deployment libraryserver --cpu-percent=70 --min=1 --max=3
+kubectl autoscale deployment libraryapi --cpu-percent=70 --min=1 --max=3
+kubectl autoscale deployment wishlistserver --cpu-percent=70 --min=1 --max=3
+kubectl autoscale deployment wishlistapi --cpu-percent=70 --min=1 --max=3
+kubectl autoscale deployment suggestionsserver --cpu-percent=70 --min=1 --max=3
+kubectl autoscale deployment suggestionsapi --cpu-percent=70 --min=1 --max=3
+kubectl autoscale deployment searchesserver --cpu-percent=70 --min=1 --max=3
+kubectl autoscale deployment searchesapi --cpu-percent=70 --min=1 --max=3
+kubectl autoscale deployment reviews-api-d --cpu-percent=70 --min=1 --max=3
+kubectl autoscale deployment reviews-server-d --cpu-percent=70 --min=1 --max=3
+
+kubectl get hpa
 
 # Add the nginx-stable Helm repository
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
