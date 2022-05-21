93e26b1b88204d26ccb5b23831a7f6d484c245d3
Martim <martimmourao@gmail.com>
2022-05-22 00:23:35 +0100

    🚀 Roles & Rolebinding Final Version 2.0

--- RBAC/utils/com.sh
+++ RBAC/utils/com.sh
@@ -4,29 +4,17 @@ openssl req -new -key adminOperationsTeam.key -out adminOperationsTeam.csr -subj
 openssl genrsa -out userManagementTeam.key 2048
 openssl req -new -key userManagementTeam.key -out userManagementTeam.csr -subj "/CN=userManagementTeam/O=userManagementTeam"
 
-
-
-
-
-kubectl config set-cluster default-cluster --server=https://<host ip>:6443 --certificate-authority <path-to-kubernetes-ca> --embed-certs
-kubectl config set-credentials <credential-name> --client-key <path-to-key>.pem --client-certificate <path-to-cert>.pem --embed-certs
-kubectl config set-context default-system --cluster default-cluster --user <credential-name>
-kubectl config use-context default-system
-
-# Check to see if I can do everything in my current namespace ("*" means all)
 kubectl auth can-i '*' '*'
-
-# Check to see if I can create pods in any namespace
 kubectl auth can-i create pods --all-namespaces
+
 cat ~/.kube/config
-kubectl get pods --namespace adminoperations
+rm -r ~/.kube/config
+
+kubectl config use-context adminOperationsTeam
 
 
 kubectl config get-contexts
 kubectl config current-context
-
-kubectl config use-context adminOperationsTeam@gke_phase4-cn_europe-west4-a_cluster-steam
-kubectl config use-context adminOperationsTeam
 
 gcloud secrets versions access 1 --secret="adminTeam-key" --format='get(payload.data)' | tr '_-' '/+' | base64 -d
 gcloud secrets versions access 1 --secret="adminTeam-cert" --format='get(payload.data)' | tr '_-' '/+' | base64 -d
