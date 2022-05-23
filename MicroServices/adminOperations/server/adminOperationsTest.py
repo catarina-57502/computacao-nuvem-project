43529af53421f7426343f06d8100acb70fc5fedd
Martim Mourão <martimmourao@gmail.com>
2022-05-23 23:28:29 +0100

    Ports error detected

--- MicroServices/adminOperations/server/adminOperationsTest.py
+++ MicroServices/adminOperations/server/adminOperationsTest.py
@@ -1,13 +1,12 @@
+from concurrent import futures
 import grpc
+
 from grpc_interceptor import ExceptionToStatusInterceptor
 from grpc_interceptor.exceptions import NotFound
 
 def serve():
     interceptors = [ExceptionToStatusInterceptor()]
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-
-    adminOperations_pb2_grpc.add_AdminOperationsServicer_to_server(AdminOperationService(), server)
-
 
     caCRT = 'ca.crt'
     serverCRT = 'server.crt'
@@ -23,13 +22,12 @@ def serve():
 
     channel_creds = grpc.ssl_server_credentials([(credsSK, credsSCRT)], credsCA, False)
 
-    server.add_secure_port("[::]:50051",channel_creds)
+    server.add_secure_port("[::]:5051",channel_creds)
     server.start()
 
     server.wait_for_termination()
 
 
 if __name__ == "__main__":
-    start_http_server(51051)
     serve()
 
