c38e34c165b085b2aeab4a7ae5e5a37fda6aa8f6
Martim <martimmourao@gmail.com>
2022-05-24 04:31:30 +0100

    Secure Channels V1.8

--- MicroServices/SSLCertificates/generate.sh
+++ MicroServices/SSLCertificates/generate.sh
@@ -1,13 +1,6 @@
-openssl req -x509 -newkey rsa:4096 -days 365 -nodes -keyout ca-key.pem -out ca-cert.pem -subj "/C=PT/ST=Lisbon/L=FCUL/O=FCUL/OU=Education/CN=adminoperationsserver"
+cfssl gencert -initca ca-csr.json | cfssljson -bare ca
 
-openssl x509 -in ca-cert.pem -noout -text
+cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -hostname='adminoperationsserver' server-csr.json | cfssljson -bare server
 
-openssl req -newkey rsa:4096 -nodes -keyout server-key.pem -out server-req.pem -subj "/C=PT/ST=Lisbon/L=FCUL/O=FCUL/OU=Education/CN=adminoperationsserver"
+cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json client-csr.json | cfssljson -bare client
 
-openssl x509 -req -in server-req.pem -days 60 -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out server-cert.pem -extfile server-ext.cnf
-
-openssl req -newkey rsa:4096 -nodes -keyout client-key.pem -out client-req.pem -subj "/C=PT/ST=Lisbon/L=FCUL/O=FCUL/OU=Education/CN=adminoperationsserver"
-
-openssl x509 -req -in client-req.pem -days 60 -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out client-cert.pem -extfile server-ext.cnf
-
-openssl x509 -in server-cert.pem -noout -text
