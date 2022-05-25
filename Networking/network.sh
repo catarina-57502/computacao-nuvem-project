b3f0cc4b2f8f7b6cc7242723b4e8ada88ed33eb4
Martim <martimmourao@gmail.com>
2022-05-25 02:29:40 +0100

    🚀 Protection for DoS (V2.8)

--- Networking/network.sh
+++ Networking/network.sh
@@ -3,11 +3,8 @@ helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
 
 
-helm install nginx-ingress ingress-nginx/ingress-nginx \
-    --set controller.publishService.enabled=true \
-    --set controller.service.externalTrafficPolicy=Local \
-    --set controller.service.annotations."service\.beta\.kubernetes\.io/do-loadbalancer-enable-proxy-protocol=true" \
-    --set-string controller.config.use-proxy-protocol=true,controller.config.use-forward-headers=true,controller.config.compute-full-forward-for=true
+helm install nginx-ingress ingress-nginx/ingress-nginx --set controller.service.externalTrafficPolicy=Local --set service.spec.externalTrafficPolicy: Local
+
 
 echo "10s"
 sleep 10s
