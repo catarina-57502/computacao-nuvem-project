7434d80c17deafcb51bcbb917fbb393f000fc50c
Martim <martimmourao@gmail.com>
2022-05-30 23:40:01 +0100

    🔧 🔨 Stress Testing Scripts

--- StressTesting/stress.py
+++ StressTesting/stress.py
@@ -1,5 +1,7 @@
 import requests
 import time
+import threading
+
 
 ip = input("Enter IP: ")
 nRequest = input("Enter Number Request Send: ")
@@ -13,9 +15,75 @@ searchesURL = "http://" + ip + "/searches/reviews"
 suggestionsURL = "http://" + ip + "/suggestions/games"
 wishesURL = "http://" + ip + "/wishes"
 
-# Token
-for i in range(int(nRequest)):
-    PARAMS = {"email":"martimmourao@gmail.com","password":"qwerty"}
-    userToken = requests.post(userManagementURL, json = PARAMS)
-    print(userToken.json())
-    time.sleep(float(timeSleep))
+
+
+PARAMS = {"email":"martimmourao@gmail.com","password":"qwerty"}
+userToken = requests.post(userManagementURL, json = PARAMS)
+userTokenFinal = userToken.json()
+
+def stressUser():
+    # Stress Testing User Management
+    for i in range(int(nRequest)):
+        PARAMS = {"email":"martimmourao@gmail.com","password":"qwerty"}
+        userToken = requests.post(userManagementURL, json = PARAMS)
+        time.sleep(float(timeSleep))
+
+def stressAdmin():
+    # Stress Testing User Management
+    for i in range(int(nRequest)):
+        PARAMS = {"email":"martimmourao@gmail.com","password":"qwerty"}
+        userToken = requests.post(userManagementURL, json = PARAMS)
+        time.sleep(float(timeSleep))
+
+def stressLib():
+    # Stress Testing User Management
+    for i in range(int(nRequest)):
+        PARAMS = {"email":"martimmourao@gmail.com","password":"qwerty"}
+        userToken = requests.post(userManagementURL, json = PARAMS)
+        time.sleep(float(timeSleep))
+
+def stressRev():
+    # Stress Testing User Management
+    for i in range(int(nRequest)):
+        PARAMS = {"email":"martimmourao@gmail.com","password":"qwerty"}
+        userToken = requests.post(userManagementURL, json = PARAMS)
+        time.sleep(float(timeSleep))
+
+def stressSea():
+    # Stress Testing User Management
+    for i in range(int(nRequest)):
+        PARAMS = {"email":"martimmourao@gmail.com","password":"qwerty"}
+        userToken = requests.post(userManagementURL, json = PARAMS)
+        time.sleep(float(timeSleep))
+
+def stressSugg():
+    # Stress Testing User Management
+    for i in range(int(nRequest)):
+        PARAMS = {"email":"martimmourao@gmail.com","password":"qwerty"}
+        userToken = requests.post(userManagementURL, json = PARAMS)
+        time.sleep(float(timeSleep))
+
+def stressWish():
+    # Stress Testing User Management
+    for i in range(int(nRequest)):
+        PARAMS = {"email":"martimmourao@gmail.com","password":"qwerty"}
+        userToken = requests.post(userManagementURL, json = PARAMS)
+        time.sleep(float(timeSleep))
+
+
+if __name__ == "__main__":
+    userM = threading.Thread(target=stressUser, args=())
+    admin = threading.Thread(target=stressAdmin, args=())
+    lib = threading.Thread(target=stressLib, args=())
+    rev = threading.Thread(target=stressRev, args=())
+    sea = threading.Thread(target=stressSea, args=())
+    sugg = threading.Thread(target=stressSugg, args=())
+    wish = threading.Thread(target=stressWish, args=())
+
+    userM.start()
+    admin.start()
+    lib.start()
+    rev.start()
+    sea.start()
+    sugg.start()
+    wish.start()
