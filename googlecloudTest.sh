18ffb33e969cdee3e2deb0acfa128b71ce0ac5d7
martim <martimmourao@gmail.com>
2022-06-23 17:50:23 +0100

    Reduce the CPU autoscale to perform the stress testing.

--- googlecloudTest.sh
+++ googlecloudTest.sh
@@ -1,7 +1,7 @@
 # Config
 export PROJECT_ID=$(gcloud info --format='value(config.project)')
 gcloud services enable cloudapis.googleapis.com  container.googleapis.com containerregistry.googleapis.com
-gcloud container clusters create cluster-steam2 --zone europe-west4-a --num-nodes 2 --enable-autoscaling --min-nodes 1 --max-nodes 3 --enable-autorepair
+gcloud container clusters create cluster-steam3 --zone europe-west4-a --num-nodes 2 --enable-autoscaling --min-nodes 1 --max-nodes 3 --enable-autorepair
 
 gcloud auth configure-docker
 
@@ -41,9 +41,58 @@ cd Networking
 chmod u+x network.sh
 ./network.sh
 
+cd ..
+cd HPA
+chmod u+x hpa.sh
+./hpa.sh
 
 cd ..
 cd Prometheus
 chmod u+x prometheus.sh
 ./prometheus.sh
 
+cd ..
+cd RBAC
+
+cd Team-AdminOperations
+chmod u+x RBAC_AdminOperations.sh
+./RBAC_AdminOperations.sh
+cd ..
+
+cd Team-UserManagement
+chmod u+x RBAC_UserManagement.sh
+./RBAC_UserManagement.sh
+cd ..
+
+cd Team-Library
+chmod u+x RBAC_Library.sh
+./RBAC_Library.sh
+cd ..
+
+cd Team-Wishlist
+chmod u+x RBAC_Wishlist.sh
+./RBAC_Wishlist.sh
+cd ..
+
+cd Team-Reviews
+chmod u+x RBAC_Reviews.sh
+./RBAC_Reviews.sh
+cd ..
+
+cd Team-Logging
+chmod u+x RBAC_Logging.sh
+./RBAC_Logging.sh
+cd ..
+
+cd Team-Searches
+chmod u+x RBAC_Searches.sh
+./RBAC_Searches.sh
+cd ..
+
+cd Team-Suggestions
+chmod u+x RBAC_Suggestions.sh
+./RBAC_Suggestions.sh
+
+cd ..
+
+cd ..
